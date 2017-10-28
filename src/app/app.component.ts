73d35ebdddef87188843a27f6c3274ef2229e6c8
thecompanydream <tbrantleyii@gmail.com>
2017-10-28 13:34:19 -0400

    initial checkin wil be continuing the tutorial from here

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+export class Hero{
+  id: number;
+  name: string;
+};
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} details!</h2>
+  <div><label>id: </label>{{hero.id}}</div>
+  <div>
+    <label>name: </label>
+    <input [(ngModel)]="hero.name" placeholder="name">
+  </div>
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Hall of heroes';
+  hero: Hero = {
+    id:1,
+    name: 'Winston'
+  };
+}
