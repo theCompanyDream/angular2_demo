61d018ae51b1375944aa0650e8e9caf739f99eb0
thecompanydream <tbrantleyii@gmail.com>
2017-11-01 17:39:07 -0400

    now the application properly routes and will react accordingly.
    P.S. God this shit is hard T_T

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroDetailComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }
+    ])
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
+    ],
+  providers: [
+    HeroService
+  ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
