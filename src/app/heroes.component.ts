985321e7005a84fe1201c7364e9018dc7235b720
thecompanydream <tbrantleyii@gmail.com>
2017-11-05 11:32:08 -0500

    heroes.component.ts uses the router to get hero details instead of managing it itself

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -17,7 +18,9 @@ export class HeroesComponent implements OnInit {
   selectedHero: Hero;
   heroes: Hero[];
 
-  constructor(private heroService: HeroService) {
+  constructor(
+    private heroService: HeroService,
+    private router: Router) {
 
   }
 
@@ -30,7 +33,7 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void{
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 
 }
