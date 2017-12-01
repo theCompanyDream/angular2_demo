0db0b6eefea410e494eceabad717052b450be4f7
thecompanydream <tbrantleyii@gmail.com>
2017-12-01 13:17:36 -0500

    hero service now uses urls to fetch urls
    crud functionaliy added to the heroservice
    hero component were changed to reflect this
    added in memory database
    made searching it's own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -9,18 +12,28 @@ import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { AppRoutingModule } from './app-routing';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    ),
     AppRoutingModule
+
   ],
   declarations: [
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
     ],
   providers: [
     HeroService
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -27,23 +27,31 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
   onselect(hero:Hero): void{
     this.selectedHero = hero;
   }
 
   getHeroes(): void {
-      this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+      this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   gotoDetail(): void{
     this.router.navigate(['/detail', this.selectedHero.id]);
-  }
-
-  searchHeroes(searchableString:string): void{
-    if(searchableString)
-      this.heroService.searchHeroes(searchableString).then(heroes => this.heroes = heroes);
-    else
-      this.getHeroes();
   }
 
 }
