0046aa537d2b54c40bb0fbef9ddadcbc058d852a
thecompanydream <tbrantleyii@gmail.com>
2017-12-03 16:22:08 -0500

    I HAVE NO IDEA WHY THIS WORKS BUT THIS SMALL CHANGES CAUSED THE API TO WORK WTF!!!!!!!!!!!!!!

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -4,6 +4,7 @@ import { HEROES } from './mock-heroes';
 
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
-    return {HEROES};
+    const heroes = HEROES;
+    return {heroes};
   }
 }
